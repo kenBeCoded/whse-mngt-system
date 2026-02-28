dafab1a58a47904c5c22827eb33335caa8bbdc9b
infinix-linux-mint <johnkennethgregorio.wom@gmail.com>
2026-02-28 19:43:40 +0800

    update sameSite to none

--- backend/src/auth/authController.ts
+++ backend/src/auth/authController.ts
@@ -68,7 +68,7 @@ router.post(
       res.cookie("refreshToken", refreshToken, {
         httpOnly: true,
         secure: process.env.NODE_ENV === "production",
-        sameSite: "strict",
+        sameSite: "none",
         maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
       });
 
